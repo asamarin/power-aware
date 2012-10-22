66ded81e764a6fbee2f5943a43869fe5e387b7cf
Alejandro Samarín <alejandro.samarin@gmail.com>
2012-10-22 03:45:39 +0100

    Added dumping to file

--- poweraware.py
+++ poweraware.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import sys
+import os
 import snmp
 import xmlparser
 import time
 
-class BladePower():
+class PowerAware():
     """ Clase para la gestion de energia del IBM BladeCenter """
     
     # Threshold value, in seconds, to distinguish fresh values from SNMP queries
@@ -35,48 +36,44 @@ class BladePower():
             self.xmlparser = xmlparser.XMLParser()
 
             if xmlsettings_file is not None:
-                self.load_blade_settings(xmlsettings_file)
+                self.load_snmp_settings(xmlsettings_file)
                 self.blade_snmp = snmp.SNMP(**self.snmp_settings)
-                #print "########"
-                #print self.power_data
-                print "Carga inicial de datos"
                 self.module_names = self.get_module_names()
-                self.update_power_data()
-                sorted_modules = self.power_data[self.POWER].keys()
-                sorted_modules.sort()
-                for module in sorted_modules:
-                    print module, ':', self.power_data[self.POWER][module]
-                test_list = ["verode11", "verode12", "verode13", "verode14", "verode15"]
-                self.test_run(test_list)
-                #print "########"
-                #print "Consumo de verode04: ", self.power_data[self.POWER]["verode04"]
+                #self.update_power_data()
+                #sorted_modules = self.power_data[self.POWER].keys()
+                #sorted_modules.sort()
+                #for module in sorted_modules:
+                #    print module, ':', self.power_data[self.POWER][module]
 
         except Exception as e:
             print >> sys.stderr, e
 
     def update_power_data(self):
-        power_domains = self.get_power_data()
-        self.power_data[self.POWER] = dict(zip(self.module_names, power_domains))
+        self.power_data[self.POWER] = dict(zip(self.module_names, self.get_power_data()))
 
-    def test_run(self, nodes):
-        print "Ctrl-C para terminar"
+    def dump(self, nodes):
+        #print "Ctrl-C para terminar"
+        if not os.path.exists('./dumps'):
+            os.mkdir('./dumps')
         t_refresh = 0
         while(True):
-            print "Refrescando datos de consumo..."
+            #print "Refrescando datos de consumo..."
             ti = time.time()
             self.update_power_data()
             tf = time.time()
             t_query = (tf - ti)
             if t_query < self.EPSILON:
-                print "T = ", t_query, " -> Peticion cacheada, descartada"
+                #print "T = ", t_query, " -> Peticion cacheada, descartada"
                 t_refresh += t_query
                 continue
-            print "T =", t_query, "-> Peticion refrescada, tiempo total =", t_refresh + t_query
-            t_refresh = 0
+            #print "T =", t_query, "-> Peticion refrescada, tiempo total =", t_refresh + t_query
             for node in nodes:
-                print node, ':', self.power_data[self.POWER][node]
+                path = './dumps/' + node + '.txt'
+                with open(path, 'a') as dumpfile:
+                    dumpfile.write("%10.3f %4.3f %d\n" % tf, t_refresh + t_query, self.power_data[self.POWER][node])
+            t_refresh = 0
 
-    def load_blade_settings(self, xmlsettings_file):
+    def load_snmp_settings(self, xmlsettings_file):
         self.xmlparser.parse_file(xmlsettings_file)
         root = self.xmlparser.get_root()
         parameters = [x.get("repr") for x in root.find("snmp")]
@@ -84,30 +81,26 @@ class BladePower():
         # Build up dictionary of SNMP session settings
         self.snmp_settings = dict(zip(parameters, values))
 
-        # Now get all the OIDs defined
+        # Now get all defined OIDs
         oids_names = [x.get("descr") for x in root.find("oids")]
         oids_values = [x.get("value") for x in root.find("oids")]
         self.oids = dict(zip(oids_names, oids_values))
 
     def get_module_names(self):
-        print "[DEBUG] Getting power domain 1 modules..."
+        #print "[DEBUG] Getting power domain 1 modules..."
         pd1modules = self.blade_snmp.walk(self.oids[self.PD1_MODULES])
-        #print pd1modules
-        print "[DEBUG] Getting power domain 2 modules..."
+        #print "[DEBUG] Getting power domain 2 modules..."
         pd2modules = self.blade_snmp.walk(self.oids[self.PD2_MODULES])
-        #print pd2modules
         return pd1modules + pd2modules
 
     def get_power_data(self):
-        #print "[DEBUG] Getting power domain 1 data..."
         powerdomain1 = self.blade_snmp.walk(self.oids[self.PD1_CURRPWR])
-        #print powerdomain1
-        #print "[DEBUG] Getting power domain 2 data..."
         powerdomain2 = self.blade_snmp.walk(self.oids[self.PD2_CURRPWR])
-        #print powerdomain2
         return powerdomain1 + powerdomain2
 
 
 if __name__ == '__main__':
-    bc = BladePower("./xml/bladecenter.xml")
+    bcenter = PowerAware("./xml/bladecenter.xml")
+    test_list = ["verode11", "verode12", "verode13", "verode14", "verode15"]
+    bcenter.dump(test_list)
     
